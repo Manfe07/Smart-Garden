d44537eee25034f144eb1769c495926d13fe863f
Manuel Fehren <Manuel.Fehren@Gmail.com>
2021-02-10 23:09:51 +0100

    added pump feature

--- data.py
+++ data.py
@@ -1,28 +1,40 @@
 import json
-json_data={
-    "Gemüsebeet":{
-        "temp": 24.5,
-        "hum": 71.4,
-        "soil": 40
-    },
-    "Tomaten": {
-        "temp": 32.5,
-        "hum": 34.4,
-        "soil": 60
-    },
-    "Zimmer": {
-        "temp": 22.5,
-        "hum": 71.4,
-        "soil": 40
-    },
-    "Zuchtkasten": {
-        "temp": 28.3,
-        "hum": 60.3,
-        "soil": 70
+def getSensors():
+    json_data={
+        "Gemüsebeet":{
+            "temp": 24.5,
+            "hum": 71.4,
+            "soil": 40
+        },
+        "Tomaten": {
+            "temp": 32.5,
+            "hum": 34.4,
+            "soil": 60
+        },
+        "Zimmer": {
+            "temp": 22.5,
+            "hum": 71.4,
+            "soil": 40
+        },
+        "Zuchtkasten": {
+            "temp": 28.3,
+            "hum": 60.3,
+            "soil": 70
+        }
     }
-}
-test_data={
-    "data1": 23123,
-    "data2": 123,
-    "data3": "fafas"
-}
+    return json_data
+
+def getPumps():
+    pumps = {
+        "Tomaten":{
+            "state": True,
+            "level": 20,
+            "percent": 70
+        },
+        "Gemüsebeet":{
+            "state": False,
+            "level": None,
+            "percent": None,
+        }
+    }
+    return pumps
