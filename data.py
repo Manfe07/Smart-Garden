838ec17d0ecb80f2e6848c2c400de08af110623b
Manuel Fehren <Manuel.Fehren@Gmail.com>
2021-02-10 21:13:52 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,28 @@
+import json
+json_data={
+    "Gemüsebeet":{
+        "temp": 24.5,
+        "hum": 71.4,
+        "soil": 40
+    },
+    "Tomaten": {
+        "temp": 32.5,
+        "hum": 34.4,
+        "soil": 60
+    },
+    "Zimmer": {
+        "temp": 22.5,
+        "hum": 71.4,
+        "soil": 40
+    },
+    "Zuchtkasten": {
+        "temp": 28.3,
+        "hum": 60.3,
+        "soil": 70
+    }
+}
+test_data={
+    "data1": 23123,
+    "data2": 123,
+    "data3": "fafas"
+}
